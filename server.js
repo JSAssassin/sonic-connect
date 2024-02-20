47bc3b259c207555ece9dc4b003621d157c5a176
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-02-20 10:32:27 -0500

    Get database user and password from env variable.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './app/routes/index.js';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-unused-vars
+mongoose.connect(process.env.LOCAL_CONN_STR).then(conn => {
+  // console.log(conn);
+  console.log('DB connection successful.');
+}).catch((error)=> {
+  console.log('Some error has occured.', error);
+});
 
 // Middleware
 app.use(express.json());
@@ -10,5 +20,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server has started on port ${port}`);
 });
