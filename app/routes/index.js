915315b9e9a4e59971252c8734037afa84f9f203
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-02-21 16:53:52 -0500

    Add `/login` and '/register' user routes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import pingRoute from './pingRoute.js';
+import userRoutes from './userRoutes.js';
 
 const router = express.Router();
 
-router.use('/rest', pingRoute);
+router.use('/', pingRoute);
+router.use('/users', userRoutes);
 
 export default router;
