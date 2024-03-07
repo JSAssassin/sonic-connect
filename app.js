cbee90031bfcaec9f94dab9433659751f48bb302
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-03-07 16:37:24 -0500

    Add default route to handle all routes that haven't been matched by previous routes.

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ app.use(express.json());
 // Routes
 app.use('/', pingRouter);
 app.use('/auth', authRouter);
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot find ${req.originalUrl} on the server.`
+  });
+});
 
 export default app;
