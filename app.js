7f9acee795cb51679f62ef55ea63284c4db5b6b2
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-03-12 11:29:25 -0400

    Add and use `globalErrorHandler()` middleware.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import {pingRouter, authRouter} from './lib/routes/index.js';
+import {authRouter, pingRouter} from './lib/routes/index.js';
+import CustomError from './lib/utils/custom-error.js';
+import globalErrorHandler from './lib/controllers/error-controller.js';
 
 const app = express();
 
@@ -8,11 +10,12 @@ app.use(express.json());
 // Routes
 app.use('/', pingRouter);
 app.use('/auth', authRouter);
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cannot find ${req.originalUrl} on the server.`
-  });
+app.all('*', (req, res, next) => {
+  const e =
+    new CustomError(`Cannot find ${req.originalUrl} on the server.`, 404);
+  next(e);
 });
+
+app.use(globalErrorHandler);
 
 export default app;
