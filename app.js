0da3cae7ad8b99d5a0b45621d8737f142e4a55c0
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-03-25 14:20:02 -0400

    Add `/updateMe` and `/deleteMe` routes.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authRouter, pingRouter } from './lib/routes/index.js';
+import { authRouter, pingRouter, userRouter } from './lib/routes/index.js';
 import CustomError from './lib/utils/custom-error.js';
 import globalErrorHandler from './lib/controllers/error-controller.js';
 
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 // Routes
 app.use('/api/v1/', pingRouter);
-app.use('/api/v1/users', authRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
   const e =
     new CustomError(`Cannot find ${req.originalUrl} on the server.`, 404);
