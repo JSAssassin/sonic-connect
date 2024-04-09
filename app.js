413b9365b1cb2ba7010459bf71d42f9256d48b8b
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-04-09 19:31:06 -0400

    Create a Photo model and add required photo property to artist, album, and song schemas.
    - Remove photo property from user schema.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use('/api/v1/', pingRouter);
