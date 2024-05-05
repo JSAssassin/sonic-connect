0855773454b252ea0fea58e9d7f10d0f03029a59
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2024-05-05 00:40:20 -0400

    Pass user and password options only if they exist.

--- initialize-db.js
+++ initialize-db.js
@@ -6,10 +6,21 @@ dotenv.config({ path: '.env' });
 const DB_NAME = process.env.NODE_ENV === 'test' ?
   `${process.env.DB_NAME}-test` : process.env.DB_NAME;
 
-const conn = await mongoose.connect(`${process.env.DB_CONN_STR}`, {
-  user: process.env.DB_USER,
-  pass: process.env.DB_PASSWORD,
+const options = {
   dbName: DB_NAME
-});
+};
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASSWORD;
+
+if(user) {
+  options.user = user;
+}
+
+if(pass) {
+  options.pass = pass;
+}
+
+const conn = await mongoose.connect(process.env.DB_CONN_STR, options);
 
 export default conn;
